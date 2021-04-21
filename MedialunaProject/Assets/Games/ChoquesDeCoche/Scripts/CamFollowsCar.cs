38038b211019b125450ac83eadb0ac54e2659923
Dysm <d.perez@colegioarula.com>
2021-04-21 17:31:52 +0200

    Mejor organización del proyecto en Unity, además de un banner menos feo.

---
+++ MedialunaProject/Assets/Games/ChoquesDeCoche/Scripts/CamFollowsCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CamFollowsCar : MonoBehaviour
+{
+    public new GameObject gameObject;
+    public GameObject camPositionObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //public GameObject camPosition = GameObject.Find("CamRig").Find("");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
